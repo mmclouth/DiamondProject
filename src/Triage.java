6c825b626e51f5d6411b367790412ba977c5ccd0
mmclouth <mackenzie-mclouth@uiowa.edu>
2017-11-15 17:40:59 -0600

    working on adding names to tasks

---
+++ src/Triage.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by kenziemclouth on 11/15/17.
+ */
+public class Triage {
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter name of file:");
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
