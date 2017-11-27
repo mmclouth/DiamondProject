db0b8a9e3dc79dc5f1639d9befdcff89b919fb5f
rhageman14 <rachel-hageman@uiowa.edu>
2017-11-27 15:53:46 -0600

    if else boolean statements

--- src/Triage.java
+++ src/Triage.java
@@ -12,6 +12,28 @@ public class Triage {
 
         Scanner input = new Scanner(System.in);
 
+        boolean bleeding = false;
+        boolean airway = false;
+        boolean maintainAirway = false;
+        boolean giveOxygen = false;
+        boolean ECG = false;
+        boolean IVaccess = false;
+        boolean cardiacConsult = false;
+        boolean fluidBolus = false;
+        boolean intubation = false;
+        boolean breathSounds = false;
+        boolean assessJugular = false;
+        boolean manualPressure = false;
+        boolean assessTissue = false;
+        boolean giveBlood = false;
+        boolean assesHiddenBleeding = false;
+        boolean CPR = false;
+        boolean hemodynamicStability = false;
+        boolean assesICP = false;
+        boolean elevateHOB = false;
+        boolean neuroConsult = false;
+
+
 
         int heartRate;
 
@@ -20,8 +42,14 @@ public class Triage {
 
         System.out.println("Heart rate is "+ heartRate);
 
-        if(heartRate > 400){
-            System.out.println("You're probably on cocaine.");
+        if(heartRate > 100 || heartRate < 60){
+            System.out.println("Perform necessary tasks");
+            maintainAirway = true;
+            giveOxygen = true;
+            ECG = true;
+            IVaccess = true;
+            cardiacConsult = true;
+
         } else {
             System.out.println("healthy");
 
@@ -30,7 +58,7 @@ public class Triage {
         System.out.print("Enter blood pressure: ");
         String bloodPressure = input.next();
 
-        System.out.println("Heart rate is "+ bloodPressure);
+        System.out.println("Blood Pressure is "+ bloodPressure);
 
 
         String[] bloodPressureSplit = bloodPressure.split("/");
@@ -38,13 +66,38 @@ public class Triage {
         System.out.println(bloodPressureSplit[0]);
         System.out.println(bloodPressureSplit[1]);
 
-        System.out.print("Is patient bleeding?");
+
+
+        System.out.print("Is patient excessively bleeding (20-40% blood loss)?");
         String response = input.next();
-        boolean bleeding = false;
+
 
         if(response.equals("yes")){
             bleeding = true;
         }
+
+        System.out.print("Is patient's airway blocked?");
+        response = input.next();
+
+        if(response.equals("yes")){
+            airway = true;
+        }
+
+        int respRate;
+
+        System.out.print("Enter respiratory rate: ");
+        respRate = Integer.parseInt(input.next());
+
+        System.out.println("Respiratory rate is "+ respRate);
+
+        if(respRate < 10 || respRate > 29){
+            System.out.println("Maintain Airway");
+        } else {
+            System.out.println("healthy");
+
+        }
+
+
 
 
     }
